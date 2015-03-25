d6bb5f9e8960fe9c69110b45b25b1aeda46cc495
dmoraschi <daniele.moraschi@gmail.com>
2015-03-25 23:38:01 +0000

    [upd]

--- create.sh
+++ create.sh
@@ -3,12 +3,12 @@
 source app.cfg
 
 
-#sudo docker pull dmoraschi/data-app
-#sudo docker pull dmoraschi/centos-php
-#sudo docker pull dmoraschi/centos-phpcomposer
-#sudo docker pull dmoraschi/centos-nginx
+sudo docker pull dmoraschi/app-volume
+sudo docker pull dmoraschi/centos-php
+sudo docker pull dmoraschi/centos-phpcomposer
+sudo docker pull dmoraschi/centos-nginx
 
-
+'''
 cd /home/dmoraschi/Sites/docker/nginx
 sudo docker build -t dmoraschi/centos-nginx .
 
@@ -20,7 +20,7 @@ sudo docker build -t dmoraschi/centos-php .
 
 cd /home/dmoraschi/Sites/docker/php-composer
 sudo docker build -t dmoraschi/centos-phpcomposer .
-
+'''
 
 
 echo "Create composer project? [symfony|silex|laravel|kohana|n]:"
@@ -28,27 +28,27 @@ read create
 
 
 sudo docker run -tid --name ${APP_NAME}-app \
-    -v ${APP_ROOT}/app:/data/app -v ${APP_ROOT}/log:/data/log dmoraschi/data-app
+    -v ${APP_ROOT}/app:/data/app dmoraschi/app-volume
 
 
 if [ $create = 'symfony' ]; then
     sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
-        --rm dmoraschi/centos-phpcomposer create-project symfony/framework-standard-edition ${APP_NAME}
+        --rm dmoraschi/centos-phpcomposer create-project symfony/framework-standard-edition .
 fi
 
 if [ $create = 'silex' ]; then
     sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
-        --rm dmoraschi/centos-phpcomposer create-project fabpot/silex-skeleton ${APP_NAME}
+        --rm dmoraschi/centos-phpcomposer create-project fabpot/silex-skeleton .
 fi
 
 if [ $create = 'laravel' ]; then
     sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
-        --rm dmoraschi/centos-phpcomposer create-project laravel/laravel ${APP_NAME}
+        --rm dmoraschi/centos-phpcomposer create-project laravel/laravel .
 fi
 
 if [ $create = 'kohana' ]; then
     sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
-        --rm dmoraschi/centos-phpcomposer create-project despark/kohana ${APP_NAME}
+        --rm dmoraschi/centos-phpcomposer create-project despark/kohana .
 fi
 
 sudo docker run --privileged=true --name ${APP_NAME}-php \
@@ -56,5 +56,6 @@ sudo docker run --privileged=true --name ${APP_NAME}-php \
 
 sudo docker run --privileged=true --name ${APP_NAME}-nginx \
     --volumes-from ${APP_NAME}-app -p ${APP_WEB}:80 --link ${APP_NAME}-php:fpm -d dmoraschi/centos-nginx
+
 
 sudo chmod 777 -R ${APP_ROOT}
