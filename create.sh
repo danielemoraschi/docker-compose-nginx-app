d1ee676906d6db1022d41ef4ffdb4f1ea5be0b4f
Daniele Moraschi <daniele.moraschi@dxi-easycall.net>
2015-12-08 19:19:01 +0000

    removed sudo for MAC OSX compatibility

--- create.sh
+++ create.sh
@@ -9,53 +9,39 @@ sudo docker pull dmoraschi/centos-phpcphalcon
 sudo docker pull dmoraschi/centos-phpcomposer
 sudo docker pull dmoraschi/centos-nginx
 
-'''
-cd /home/dmoraschi/Sites/docker/nginx
-sudo docker build -t dmoraschi/centos-nginx .
-
-cd /home/dmoraschi/Sites/docker/data
-sudo docker build -t dmoraschi/data-app .
-
-cd /home/dmoraschi/Sites/docker/php
-sudo docker build -t dmoraschi/centos-php .
-
-cd /home/dmoraschi/Sites/docker/php-composer
-sudo docker build -t dmoraschi/centos-phpcomposer .
-'''
-
 
 echo "Create composer project? [symfony|silex|laravel|kohana|n]:"
 read create
 
 
-sudo docker run -tid --name ${APP_NAME}-app \
+docker run -tid --name ${APP_NAME}-app \
     -v ${APP_ROOT}/app:/data/app dmoraschi/app-volume
 
 
 if [ $create = 'symfony' ]; then
-    sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
+    docker run --privileged=true --volumes-from ${APP_NAME}-app \
         --rm dmoraschi/centos-phpcomposer create-project symfony/framework-standard-edition .
 fi
 
 if [ $create = 'silex' ]; then
-    sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
+    docker run --privileged=true --volumes-from ${APP_NAME}-app \
         --rm dmoraschi/centos-phpcomposer create-project fabpot/silex-skeleton .
 fi
 
 if [ $create = 'laravel' ]; then
-    sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
+    docker run --privileged=true --volumes-from ${APP_NAME}-app \
         --rm dmoraschi/centos-phpcomposer create-project laravel/laravel .
 fi
 
 if [ $create = 'kohana' ]; then
-    sudo docker run --privileged=true --volumes-from ${APP_NAME}-app \
+    docker run --privileged=true --volumes-from ${APP_NAME}-app \
         --rm dmoraschi/centos-phpcomposer create-project despark/kohana .
 fi
 
-sudo docker run --privileged=true --name ${APP_NAME}-php \
+docker run --privileged=true --name ${APP_NAME}-php \
     --volumes-from ${APP_NAME}-app -d dmoraschi/centos-phpcphalcon
 
-sudo docker run --privileged=true --name ${APP_NAME}-nginx \
+docker run --privileged=true --name ${APP_NAME}-nginx \
     --volumes-from ${APP_NAME}-app -p ${APP_WEB}:80 --link ${APP_NAME}-php:fpm -d dmoraschi/centos-nginx
 
 
--- delete.sh
+++ delete.sh
@@ -9,10 +9,10 @@ if [ $remove = 'y' ]; then
     sudo rm -Rf ${APP_ROOT}/*
 fi
 
-sudo docker kill ${APP_NAME}-app
-sudo docker kill ${APP_NAME}-php
-sudo docker kill ${APP_NAME}-nginx
+docker kill ${APP_NAME}-app
+docker kill ${APP_NAME}-php
+docker kill ${APP_NAME}-nginx
 
-sudo docker rm ${APP_NAME}-app
-sudo docker rm ${APP_NAME}-php
-sudo docker rm ${APP_NAME}-nginx
+docker rm ${APP_NAME}-app
+docker rm ${APP_NAME}-php
+docker rm ${APP_NAME}-nginx
